73f44363dcebbd185b6e445304f14ccc3d11af6d
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2023-10-19 21:18:17 +0600

    2-solve-problem

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# insta-shohor
+### "insta-shohor" file name ###
 
 
 
--- js/app.js
+++ js/app.js
@@ -51,6 +51,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
+  console.log(post);
     const image = post.image;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
@@ -62,7 +63,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
@@ -120,9 +121,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                           ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
@@ -159,8 +160,8 @@ const displayReportedPosts = () => {
 };
 
 const loadPosts = async () =>{
-  let data = await fetch('https://raw.githubusercontent.com/ProgrammingHero1/insta-shohor/main/data/posts.json');
-  posts = await data.json();
+  const data = await fetch('data/posts.json');
+  const posts = await data.json();
   showPosts(posts);
 }
 
